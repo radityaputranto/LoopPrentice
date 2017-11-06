a48432687c76c6721bf84ad02d1066621490adf0
radityaputranto <radityaputranto97@yahoo.co.id>
2017-11-06 09:45:31 +0700

    versi 1.0.3 table,posting input

--- views/privatearea_prentice/index.php
+++ views/privatearea_prentice/index.php
@@ -106,34 +106,49 @@
         </div>
       </div>
 
-      <!-- banner carousel -->
-      <div class="row">
-        <div class="col-lg-12">
-          <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-              <div class="carousel-inner">
-                <div class="carousel-item active">
-                  <img class="d-block w-100" src="libs/home_register/img/banner1.jpg" alt="First slide">
-                </div>
-                <div class="carousel-item">
-                  <img class="d-block w-100" src="libs/home_register/img/banner2.jpg" alt="Second slide">
-                </div>
-                <div class="carousel-item">
-                  <img class="d-block w-100" src="libs/home_register/img/banner3.jpg" alt="Third slide">
-                </div>
-              </div>
-              <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-              </a>
-            </div>
-        </div>
-      </div>
-      <!-- Area Chart Example-->
-     
+      <!-- tabel psting user -->
+      <div class="table-responsive">
+            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+              <thead>
+                <tr>
+                  <th>Judul Posting</th>
+                  <th>Tanggal Posting</th>
+                  <th>Tanggal Publish</th>
+                  <th>Tanggal Edit</th>
+                  <th>Edit</th>
+                  <th>Delete</th>
+                </tr>
+              </thead>
+              <tfoot>
+                <tr>
+                  <th>Judul Posting</th>
+                  <th>Tanggal Posting</th>
+                  <th>Tanggal Publish</th>
+                  <th>Tanggal Edit</th>
+                  <th>Edit</th>
+                  <th>Delete</th>
+                </tr>
+              </tfoot>
+              <tbody>
+                <tr>
+                  <td>Te posting 123</td>
+                  <td>3/11/2017</td> 
+                  <td>1/11/2017</td>
+                  <td>-</td>
+                  <td>
+                    <button type="button" class="btn btn-primary">Edit</button>
+                  </td>
+                  <td>
+                    <button type="button" class="btn btn-danger">Delete</button>
+                  </td>
+                </tr>
+                
+              </tbody>
+            </table>
+          </div>
+      <!-- end-tabel psting user -->
+
+      
       
     <!-- /.container-fluid-->
     <!-- /.content-wrapper-->
